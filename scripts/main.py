bfa84bc3f1c16cbbfe133330614d62abf7e4d5a1
Piotr Chmielewski <pchmielewski6@gmail.com>
2019-06-16 12:28:10 +0200

    added csv writer

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,13 @@
 import eventlet
+
 eventlet.monkey_patch()
 import sys
 import requests
+import csv
 from statistics import mean
 from timeit import default_timer as timer
 
 PROBE_NUM = 10
-
-
-def fetch(url):
-    r = requests.get(url)
-    # print(r.text)
 
 
 def test_performance(queries_number):
@@ -20,11 +17,20 @@ def test_performance(queries_number):
     for i in range(PROBE_NUM):
         start = timer()
         for _ in range(queries_number):
-            pool.spawn(fetch, url)
+            pool.spawn(requests.get, url)
         pool.waitall()
         end = timer()
         time_probes.append(end - start)
-    print(f"Mean time equals {mean(time_probes)} seconds for {queries_number} queries")
+    result = mean(time_probes)
+    print(f"Mean time equals {result} seconds for {queries_number} queries")
+    return result
+
+
+def save_csv(data, file_name):
+    with open(file_name, "w+", newline='\n') as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for d in data:
+            csv_writer.writerow(["{:2.3f}".format(d)])
 
 
 def main():
@@ -32,8 +38,11 @@ def main():
         print("You must provide number of queries to perform!")
         exit(-1)
     try:
-        n = int(sys.argv[1])
-        test_performance(n)
+        results = []
+        for n in sys.argv[1:]:
+            n = int(n)
+            results.append(test_performance(n))
+        save_csv(results, f"results.csv")
     except ValueError:
         print("Queries number must be an integer!")
 
